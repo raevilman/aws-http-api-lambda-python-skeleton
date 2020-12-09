1eed34e19bf4e553075684a2ddf4f55f6c4db5ac
Raman Dhawan <8374679+raevilman@users.noreply.github.com>
2020-12-09 20:43:56 +0530

    📤 Adds new methods to reply back

--- app/responder.py
+++ app/responder.py
@@ -13,9 +13,23 @@ def send_ok_response(body: str):
     return send_response(200, body)
 
 
+def send_error(status: int, msg: str, req_id: str):
+    error = get_error_body(msg, req_id)
+    return send_response(status, json.dumps(error))
+
+
 def send_server_error(req_id: str):
-    error = {
-        'error': "Something went wrong",
-        'req_id': req_id
-    }
+    error = get_error_body(msg="Something went wrong", req_id=req_id)
     return send_response(500, json.dumps(error))
+
+
+def get_error_body(msg: str, req_id: str):
+    return json.dumps({
+        'error': msg,
+        'req_id': req_id
+    })
+
+
+def send_header_missing_error(header_key: str, req_id: str):
+    error_msg = "'" + header_key + "' header is missing!"
+    return send_response(status=401, body=get_error_body(error_msg, req_id))
